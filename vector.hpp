313a89856aad593b673500279cdfbbc178236536
yfoucade <yohan.foucade@gmail.com>
2022-12-26 22:13:25 +0100

    class vector: dfine member types

--- vector.hpp
+++ vector.hpp
@@ -6,10 +6,39 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 11:25:25 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/12/26 11:25:55 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/12/26 15:48:21 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <memory>
+
 namespace ft{
-	
+
+template < typename T, typename Allocator = std::allocator<T> >
+class vector{
+	private:
+		typedef T value_type;
+		typedef Allocator allocator_type;
+		typedef std::size_t size_type;
+		typedef std::ptrdiff_t difference_type;
+		typedef value_type& reference;
+		typedef const value_type& const_reference;
+		typedef typename Allocator::pointer pointer;
+		typedef typename Allocator::const_pointer const_pointer;
+		typedef value_type* iterator;
+		typedef const value_type* const_iterator;
+		typedef std::reverse_iterator<iterator> reverse_iterator;
+		typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+
+		value_type* _tab;
+		allocator_type _allocator;
+
+	public:
+		vector( void ):_tab(_allocator.allocate(0)){}
+		explicit vector( const Allocator& alloc ): _allocator(alloc), _tab(_allocator.allocate(0)){}
+		
+};
+
 }
