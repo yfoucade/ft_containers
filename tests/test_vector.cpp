3158633f8ed31676e404a3f5a556cbbae9145b72
yfoucade <yohan.foucade@gmail.com>
2022-12-26 22:13:48 +0100

    create test file for vectors

---
+++ tests/test_vector.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_vector.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/26 13:58:39 by yfoucade          #+#    #+#             */
+/*   Updated: 2022/12/26 15:06:46 by yfoucade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <vector>
+#include "vector.hpp"
+
+void test_constructor(void){
+	std::vector<int> v1;
+	ft::vector<int> v2;
+	
+}
+
+int main(void){
+	
+}
