9abb28ebd74ec36ee84d644c16a7ca40ec1e7c34
yfoucade <yohan.foucade@gmail.com>
2023-01-17 05:31:30 +0100

    refactor is_integral: add integral_constant, true_type, and false_type

--- is_integral.hpp
+++ is_integral.hpp
@@ -6,7 +6,7 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 21:17:24 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/12/31 21:37:35 by yfoucade         ###   ########.fr       */
+/*   Updated: 2023/01/17 04:35:14 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,53 +15,48 @@
 namespace ft
 {
 
+template< class T, T v >
+struct integral_constant
+{
+	typedef T value_type;
+	typedef struct integral_constant<T, v> type;
+	static const T value = v;
+};
+
+typedef integral_constant<bool, true> true_type;
+typedef integral_constant<bool, false> false_type;
+
 template< typename T >
-struct is_integral
-{
-	static const bool value = false;
-};
+struct is_integral: false_type
+{};
 
 template<>
-struct is_integral<bool>
-{
-	static const bool value = true;
-};
+struct is_integral<bool>: true_type
+{};
 
 template<>
-struct is_integral<char>
-{
-	static const bool value = true;
-};
+struct is_integral<char>: true_type
+{};
 
 template<>
-struct is_integral<wchar_t>
-{
-	static const bool value = true;
-};
+struct is_integral<wchar_t>: true_type
+{};
 
 template<>
-struct is_integral<short>
-{
-	static const bool value = true;
-};
+struct is_integral<short>: true_type
+{};
 
 template<>
-struct is_integral<int>
-{
-	static const bool value = true;
-};
+struct is_integral<int>: true_type
+{};
 
 template<>
-struct is_integral<long>
-{
-	static const bool value = true;
-};
+struct is_integral<long>: true_type
+{};
 
 template<>
-struct is_integral<long long>
-{
-	static const bool value = true;
-};
+struct is_integral<long long>: true_type
+{};
 
 
 } // namespace ft
