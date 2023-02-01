ec34973f775bb6b9097f13e6f4f618f13e5cc8f9
yfoucade <yohan.foucade@gmail.com>
2023-02-01 16:45:58 +0100

    show all tests

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -6,7 +6,7 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 13:58:39 by yfoucade          #+#    #+#             */
-/*   Updated: 2023/01/19 17:12:58 by yfoucade         ###   ########.fr       */
+/*   Updated: 2023/02/01 16:33:12 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 #include "tests.hpp"
 
 int main(void){
-	// test_constructors();
-	// test_access();
+	test_constructors();
+	test_access();
 	test_iterators();
-	// test_capacity();
-	// test_modifiers();
-	// test_non_member();
+	test_capacity();
+	test_modifiers();
+	test_non_member();
 }
