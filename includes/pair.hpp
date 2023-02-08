b5aac27a6af690e76a7a94172fe65eb01a4cd3c2
yfoucade <yfoucade@student.42.fr>
2023-02-08 14:23:15 +0100

    fix: assignment operator is implicitly declared

--- includes/pair.hpp
+++ includes/pair.hpp
@@ -6,7 +6,7 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 12:00:15 by yfoucade          #+#    #+#             */
-/*   Updated: 2023/01/26 19:19:05 by yfoucade         ###   ########.fr       */
+/*   Updated: 2023/02/08 12:26:45 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ struct pair
 	pair( const T1& x, const T2& y );
 	template< class U1, class U2 >
 	pair( const pair<U1, U2>& p );
-	pair& operator=( const pair& other );
 };
 
 template< typename T1, typename T2 >
@@ -38,18 +37,6 @@ pair<T1, T2>::pair( const T1& x, const T2& y ): first(T1(x)), second(T2(y)){}
 template< typename T1, typename T2 >
 template< typename U1, typename U2 >
 pair<T1, T2>::pair( const pair<U1, U2>& p ): first(T1(p.first)), second(T2(p.second)){}
-
-template< typename T1, typename T2 >
-pair<T1, T2>& pair<T1, T2>::operator=( const pair& other )
-{
-	if (this == &other)
-		return *this;
-	first.~T1();
-	second.~T2();
-	first = T1(other.first);
-	second = T2(other.second);
-	return *this;
-}
 
 } // namespace ft
 
